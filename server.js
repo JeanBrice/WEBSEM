cc7bbdb295f075af150aa9abd7e795cdb354d1a5
Jeremy PASCAL <jr28.my.p@gmail.com>
2018-05-23 15:23:52 +0200

    API

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+var router = express.Router();
+
+// DATABASE CONFIGURATION ================================================
+const SparqlClient = require('sparql-client-2');
+const SPARQL = SparqlClient.SPARQL;
+const endpoint = ''; // OUR DATABASE
 
 // CONFIGURATION =========================================================
 var port = process.env.PORT || 8080;
@@ -18,7 +24,48 @@ var server = app.listen(port, () => {
 });
 
 // ROUTES CONFIGURATION FOR OUR API ======================================
-// require('./app/routes/routes')(app, io);
+app.use('/api', router);
+
+router.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the WEBSEM PROJECT API!' });
+});
+
+// ROUTE FOR /scenes -----------------------------------------------------
+app.get('/api/scenes', (req, res) => {
+    
+    // Get all scenes and the film name associated to it
+    var query =
+        SPARQL  `PREFIX: <http://www.semanticweb.org/localuser/ontologies/2018/4/untitled-ontology-5/>
+                SELECT ?title ?scene ?lat ?long
+                WHERE{
+                    ? scene : takesPlaceIn : New_York .
+                    ?film : contains ?scene .
+                    ?film : has_for_title ?title .
+                    ?scene : has_longitude ?long .
+                    ?scene : has_latitude ?lat
+                }`
+    
+    var client = new SparqlClient(endpoint);
+        // .register({ db: 'http://dbpedia.org/resource/' })
+        // .register({ dbpedia: 'http://dbpedia.org/property/' });
+
+    client.query(query)
+        .execute()
+        .then(function (results) {
+            console.dir(results, { depth: null });
+            res.json(results);
+        })
+        .catch(function (error) {
+            // Oh no! 🙀
+            console.log(error);
+        });
+
+});
+
+// ROUTE FOR /films/:film_title ------------------------------------------
+app.get('/api/films/:film_title', (req, res) => {
+    res.json({ message: 'Welcome to the WEBSEM PROJECT API!' });
+});
 
 // SERVE STATIC FILES 
 app.use(express.static(path.join(__dirname, 'front/')));
